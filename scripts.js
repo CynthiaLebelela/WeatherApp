2667237a78dba09527c75ebae1b3857f823c070c
Cynthia <135084120+CynthiaLebelela@users.noreply.github.com>
2024-08-25 17:09:03 +0200

    Added functionality to the search button
    enable to change weather unit  in whole number form, and correct lower & upper case on a word(city searched).

--- scripts.js
+++ scripts.js
@@ -1,12 +1,30 @@
+//Update weather information on the right-side & it  also correct the the use of lower and uppercase in a word
+function updateWeatherInfo(response){
+    let tempEle = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+     let cityEle = document.querySelector("#city");
+    cityEle.innerHTML = response.data.city;
+    tempEle.innerHTML = Math.round(temperature);
+}
+
+//Make API call and updatethe user interface
+function searchCity(city){
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 //Changes the h1 based on search-input
 function changeH1(event){
     event.preventDefault();
     let searchSpace = document.querySelector("#search-input");
-    console.log(searchSpace.value);
 
-    let cityEle = document.querySelector("#city");
-    cityEle.innerHTML = searchSpace.value;
+    searchCity(searchSpace.value);
 }
 //Call the h1 changes function
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", changeH1);
+
+//search city by current location
+//searchCity();
