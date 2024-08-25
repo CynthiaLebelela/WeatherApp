43f5fe40e5218b99c972e7c983aaf76196bb2b37
Cynthia <135084120+CynthiaLebelela@users.noreply.github.com>
2024-08-25 19:18:10 +0200

    Edited javascript
    removed all the static information and used javascript to provide the dynamic one.

--- scripts.js
+++ scripts.js
@@ -2,8 +2,22 @@
 function updateWeatherInfo(response){
     let tempEle = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-     let cityEle = document.querySelector("#city");
+    let cityEle = document.querySelector("#city");
+    let descriptionEle = document.querySelector("#description");
+    let humidityEle = document.querySelector("#humidity");
+    let windEle = document.querySelector("#wind-speed");
+    let dateTimeEle = document.querySelector("#datetime");
+    let date = new Date(response.data.time * 1000);
+    let iconEle = document.querySelector("#icon");
+    
+    
+    iconEle.innerHTML = ` <img src="${response.data.condition.icon_url}" class="weather-app-icon" style" width="80px;"/> `
+     console.log(response.data.wind.speed);
     cityEle.innerHTML = response.data.city;
+    descriptionEle.innerHTML = response.data.condition.description;
+    humidityEle.innerHTML = `${response.data.temperature.humidity}%`;
+    windEle.innerHTML = `${response.data.wind.speed}km/h`;
+    dateTimeEle.innerHTML = formatDate(date);
     tempEle.innerHTML = Math.round(temperature);
 }
 
@@ -13,6 +27,19 @@ function searchCity(city){
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(updateWeatherInfo);
+}
+
+//format date and time
+function  formatDate(date){
+   let hour = date.getHours();
+   let minutes = date.getMinutes();
+   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   let day = days[date.getDay()];
+
+   if (minutes < 10){
+    minutes = `0${minutes}`;
+   }
+   return `${day} ${hour}:${minutes}`;
 }
 
 //Changes the h1 based on search-input
@@ -26,5 +53,7 @@ function changeH1(event){
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", changeH1);
 
-//search city by current location
+//To-Do List
+//search city by current location 
 //searchCity();
+//Make the first letter a capital letter on the description
